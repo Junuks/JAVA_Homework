cf89d109a134b1c7e506a74b83156b169aad128e
Junuks <48515527+Junuks@users.noreply.github.com>
2019-03-13 21:07:33 +0900

    d
    d

---
+++ MyInfoApp.java
@@ -0,0 +1,12 @@
+
+public class MyInfoApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Kitae Hwang");
+		System.out.println("20 years old");
+		System.out.println("Department of Computer Engineering");
+		
+	}
+
+}
